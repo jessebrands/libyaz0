33ae4bcbb25e12ace66feb541ae53436b67d8792
Jesse Gerard Brands <jesse@jgbrands.nl>
2026-07-30 12:11:39 +0200

    Add allocator

---
+++ src/common.c
@@ -0,0 +1,73 @@
+/* common.c: common library utilities and allocator
+   Copyright (C) 2026 Jesse Gerard Brands
+
+   This file is part of libyaz0.
+
+   libyaz0 is free software: you can redistribute it and/or modify it under
+   the terms of the GNU Lesser General Public License as published by the Free
+   Software Foundation, either version 3 of the License, or (at your option)
+   any later version.
+
+   libyaz0 is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+   more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with libyaz0. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <assert.h>
+#include <stdlib.h>
+
+#include "common.h"
+
+static struct yaz0_common_state*
+yaz0_get_common_state(struct yaz0_stream const* stream) {
+    assert(stream != NULL);
+
+    if (stream == NULL || stream->state == NULL) {
+        return NULL;
+    }
+    return stream->state;
+}
+
+void*
+yaz0_alloc(struct yaz0_stream const* stream, size_t const size) {
+    assert(stream != NULL);
+
+    if (stream == NULL) {
+        return NULL;
+    }
+
+    if (stream->state == NULL && stream->alloc != NULL) {
+        return stream->alloc(stream->opaque, size);
+    }
+
+    struct yaz0_common_state const* state = yaz0_get_common_state(stream);
+    if (state != NULL && state->alloc != NULL) {
+        return state->alloc(stream->opaque, size);
+    }
+
+    return malloc(size);
+}
+
+void
+yaz0_free(struct yaz0_stream const* stream, void* ptr) {
+    assert(stream != NULL);
+
+    if (stream == NULL) {
+        return;
+    }
+
+    if (stream->state == NULL && stream->free != NULL) {
+        return stream->free(stream->opaque, ptr);
+    }
+
+    struct yaz0_common_state const* state = yaz0_get_common_state(stream);
+    if (state != NULL && state->alloc != NULL) {
+        return state->free(stream->opaque, ptr);
+    }
+
+    return free(ptr);
+}
---
+++ src/common.h
@@ -0,0 +1,39 @@
+/* common.h: common library utilities and allocator
+   Copyright (C) 2026 Jesse Gerard Brands
+
+   This file is part of libyaz0.
+
+   libyaz0 is free software: you can redistribute it and/or modify it under
+   the terms of the GNU Lesser General Public License as published by the Free
+   Software Foundation, either version 3 of the License, or (at your option)
+   any later version.
+
+   libyaz0 is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+   more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with libyaz0. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBYAZ0_COMMON_H
+#define LIBYAZ0_COMMON_H
+
+#include <stddef.h>
+
+#include "yaz0/yaz0.h"
+
+struct yaz0_common_state {
+    struct yaz0_stream* stream;
+
+    void* opaque;
+    yaz0_alloc_func alloc;
+    yaz0_free_func free;
+};
+
+void* yaz0_alloc(struct yaz0_stream const* stream, size_t size);
+
+void yaz0_free(struct yaz0_stream const* stream, void* ptr);
+
+#endif //LIBYAZ0_COMMON_H
