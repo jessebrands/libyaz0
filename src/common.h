f080a501c69ddebfe66145ea899f005d0548f60d
Jesse Gerard Brands <jesse@jgbrands.nl>
2026-08-08 22:57:35 +0200

    Modify file headers in library

--- src/common.h
+++ src/common.h
@@ -1,21 +1,22 @@
-/* common.h: common library utilities and allocator
-   Copyright (C) 2026 Jesse Gerard Brands
-
-   This file is part of libyaz0.
-
-   libyaz0 is free software: you can redistribute it and/or modify it under
-   the terms of the GNU Lesser General Public License as published by the Free
-   Software Foundation, either version 3 of the License, or (at your option)
-   any later version.
-
-   libyaz0 is distributed in the hope that it will be useful, but WITHOUT ANY
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-   FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-   more details.
-
-   You should have received a copy of the GNU Lesser General Public License
-   along with libyaz0. If not, see <https://www.gnu.org/licenses/>.
-*/
+/*
+ * common.h: common library utilities and allocator
+ * Copyright (C) 2026 Jesse Gerard Brands
+ *
+ * This file is part of libyaz0.
+ *
+ * libyaz0 is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * libyaz0 is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libyaz0. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #ifndef LIBYAZ0_COMMON_H
 #define LIBYAZ0_COMMON_H
--- src/compress.c
+++ src/compress.c
@@ -1,21 +1,22 @@
-/* compress.c: Yaz0 compressor implementation
-   Copyright (C) 2026 Jesse Gerard Brands
-
-   This file is part of libyaz0.
-
-   libyaz0 is free software: you can redistribute it and/or modify it under
-   the terms of the GNU Lesser General Public License as published by the Free
-   Software Foundation, either version 3 of the License, or (at your option)
-   any later version.
-
-   libyaz0 is distributed in the hope that it will be useful, but WITHOUT ANY
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-   FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-   more details.
-
-   You should have received a copy of the GNU Lesser General Public License
-   along with libyaz0. If not, see <https://www.gnu.org/licenses/>.
-*/
+/*
+ * compress.c: Yaz0 compressor
+ * Copyright (C) 2026 Jesse Gerard Brands
+ *
+ * This file is part of libyaz0.
+ *
+ * libyaz0 is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * libyaz0 is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libyaz0. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <assert.h>
 #include <stdbool.h>
